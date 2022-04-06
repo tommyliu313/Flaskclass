8bfe1408aec3c66c37681271e7496954124907da
Tommyliu313 <[liuchaktin@gmail.com]>
2022-04-07 00:26:06 +0800

    20220407 0026 update

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect
+from config import Config
+from forms import LoginForm
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    user = {'username':'Miguel'}
+    posts = [
+        {"author": {'username':'John'},'body':'The Avengers movie was so cool!'},
+        {"author": {'username': 'Susan'}, 'body': 'Beautiful day in Portland!'}
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
-
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login Succeed for user {}, remember_me={}').format(form.username.data,form.remember_me.data)
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
 if __name__ == '__main__':
     app.run()
