d700aa9fdf366b37c36aa1afc329692ee26ecba1
Tommyliu313 <[liuchaktin@gmail.com]>
2022-04-07 10:42:00 +0800

    20220407 1041 update

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user,current_user,login_required
 from werkzeug.urls import url_parse
-from app import app
+from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
@@ -25,10 +25,34 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-
-        flash('Login Succeed for user {}, remember_me={}').format(form.username.data,form.remember_me.data)
-        return redirect('/index')
+        user = User.query.filter_by(username=form.username).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user,remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,email=form.email.data)
+        user .set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Sign In', form=form)
 if __name__ == '__main__':
     app.run()
