cbad21de4cbce9ce07312c9581b79f022a447160
Tommyliu313 <[liuchaktin@gmail.com]>
2022-04-07 13:55:24 +0800

    20220407 1355 update

--- app/routes.py
+++ app/routes.py
@@ -86,6 +86,14 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html',title='Edit Profile',form=form)
 
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {] not found.' .format(username))
+        return redirect(url_for('index'))
+    if
 
 
 if __name__ == '__main__':
