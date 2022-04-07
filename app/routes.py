4dfc21e7e2bdc96bf0a549cde0207872be317ce1
Tommyliu313 <[liuchaktin@gmail.com]>
2022-04-07 12:06:06 +0800

    20220407 update

--- app/routes.py
+++ app/routes.py
@@ -54,5 +54,6 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Sign In', form=form)
+
 if __name__ == '__main__':
     app.run()
