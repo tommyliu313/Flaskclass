a5d83c374bcf762ba8d02ea00760f33efbfceab4
Tommyliu313 <[liuchaktin@gmail.com]>
2022-04-07 13:25:02 +0800

    20220407 1324 update

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,23 @@ if not app.debug:
         secure = None
         if app.config['MAIL_USE_TLS']:
             secure = ()
-        mail_handler = SMTPHandler()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER', app.config['MAIL_PORT']]),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'],subject='Microblog Failure',
+            credentials=auth,secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log',maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
 
-from app import routes, models
+    app.logger.addHandler(file_handler)
+    app.logger.info('Microblog startup')
+
+
+from app import routes, models, errors
