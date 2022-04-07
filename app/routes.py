9c948cbb6310906a7a19cf3ef0ec165e3f4214c2
Tommyliu313 <[liuchaktin@gmail.com]>
2022-04-07 10:18:28 +0800

    20220407 1018 update

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, flash, redirect
+from flask import Flask, render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user,current_user,login_required
+from werkzeug.urls import url_parse
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -17,8 +21,11 @@ def index():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+
         flash('Login Succeed for user {}, remember_me={}').format(form.username.data,form.remember_me.data)
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
