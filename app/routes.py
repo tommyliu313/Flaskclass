f50a398653bcd6efd268e53986829997841fccf6
Tommyliu313 <[liuchaktin@gmail.com]>
2022-04-08 13:37:43 +0800

    20220408 1337 update

--- app/routes.py
+++ app/routes.py
@@ -16,14 +16,21 @@ def before_request():
 @app.route('/index',methods=['GET','POST'])
 @login_required
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {"author": {'username':'John'},
-         'body':'The Avengers movie was so cool!'},
-        {"author": {'username': 'Susan'},
-         'body': 'Beautiful day in Portland!'}
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data,author=current_user)
+        db.session.add(Post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    page = request.args.get('page',1,type=int)
+    posts = current_user.followed_posts().paginate(
+        page,app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index',page = posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index',page = posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form, posts=posts.items,next_url=next_url,prev_url=prev_url)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -121,7 +128,11 @@ def unfollow(username):
 def explore():
     page = request.args.get('page',1,type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
-
-    )
+    page,app.config['POSTS_PER_PAGE'],False)
+    next_url = url_for('explore',page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore',page=posts.prev_num) \
+        if posts.has_next else None
+    return render_template('index.html',title='Explore',posts=posts.items,next_url=next_url,prev_url=prev_url)
 if __name__ == '__main__':
     app.run()
